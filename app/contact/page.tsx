5035f77e00ee931eb0dcc92594420319d2d90b20
Badbloodyt <Prateekpandey20jan@gmail.com>
2026-01-28 03:30:44 +0530

    Add Contact page with contact information

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+export default function Contact() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Contact Us</h1>
+
+      <p>
+        📞 Phone: 7355449247 <br />
+        📧 Email: ambitionacademy@email.com
+      </p>
+
+      <p>
+        📍 Address: Ambition Academy, India
+      </p>
+    </main>
+  );
+}
